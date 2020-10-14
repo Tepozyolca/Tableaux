392d24fcfdfc23769b6376dcb34b2ff298a355c2
Tepozyolca <71712439+Tepozyolca@users.noreply.github.com>
2020-10-14 11:20:26 +0200

    Add files via upload

--- Table de multiplication.php
+++ Table de multiplication.php
@@ -1,31 +1,32 @@
 <html>
 <body>
 	<?php 
-		echo '<table>';
+		echo '<table>'; //On crée le tableau en html.
 	
-		for ($varx = -1; $varx < 13; $varx++) 
+		for ($varx = -1; $varx < 13; $varx++) //On initialise les lignes.
+											  //En outre, on commence à -1 pour pouvoir ajuster les valeurs de base des produits.
 		{
-			echo '<tr>';
-			for ($vary = -1; $vary < 13; $vary++) 
+			echo '<tr>'; //Pour laisser le coin en haut, à gauche, vide.
+			for ($vary = -1; $vary < 13; $vary++) //On initialise les colonnes.
 			{
 				echo '<td>';
-				if ($varx == -1)
+				if ($varx == -1) //Première ligne.
 				{
-					if ($vary == -1)
+					if ($vary == -1) 
 						echo null;
 					else
 						echo $vary;
 				}
 				else
 				{	
-					if ($varx != -1 && $vary == -1)
+					if ($varx != -1 && $vary == -1) //Première colonne.
 						echo $varx;
 					else
 						echo $varx*$vary;
 				echo '</td>';
 				}
 			}
-			$vary=0;
+			$vary=0; //On réinitialise y pour pouvoir afficher la valeur des produits de la ligne suivante.
 			echo '</tr>';
 		}	
 	
