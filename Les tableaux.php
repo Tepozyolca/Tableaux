35e8d66f9fbe8e6c1b4d3120fe53bcc547ad48b8
Tepozyolca <71712439+Tepozyolca@users.noreply.github.com>
2020-10-14 11:43:27 +0200

    Add files via upload

--- Les tableaux.php
+++ Les tableaux.php
@@ -8,24 +8,18 @@
 	
 	//exercice 1
 	
-	foreach($a["19002"] as $result => $resultat) //On bouge le curseur sur le groupe 19002.
-	{
 		$resultat = array_search('Validation', $a["19002"]) +1; //On cherche la première instance de 'Validation' avec la fonction 'array_search'.
-	}
 	
 	echo "La validation du groupe 19002 se passe à la semaine ".$resultat.".<br><br>";
 	
 	//exercice 2
 	
-	foreach($a["19001"] as $result => $resultat)
-	{
 		$result = array_reverse($a["19001"]); //On inverse le tableau pour pouvoir utiliser 'array_search' ,
 											  //pour que la dernière instance de stage soit la première dans le tableau tampon.
 											  
 		$resultat = count($result) - array_search('Stage', $result) +1; //On récupère le nombre total de valeurs dans le tableau du groupe 19001,
 																		//puis, vu que le résultat de la commande est basée sur le tableau mais inversé,
 																		//on soustrait la valeur de la clé obtenu pour avoir la bonne valeur
-	}
 	
 	echo "La dernière occurrence de Stage pour le groupe 19001 est la semaine ".$resultat.".<br><br>";
 	
@@ -37,7 +31,7 @@
 	//exercice 4
 	
 	$abis = array_keys($a["19003"], "Stage"); //On crée un tableau alternatif, contenant uniquement les instances de stages.
-	$resultat = count($abis); //On compte le nombre de valeur du tableau généré si dessus.
+	$resultat = count($abis); //On compte le nombre de valeur du tableau généré ci dessus.
 	
 	echo "Le stage du groupe 19003 dure ".$resultat." semaines.";
 	?>
